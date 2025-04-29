ad83d1d05c9f07c66b36d5ccf354106c900ef3ba
dyl9874 <159584716+Syntax-Soldier@users.noreply.github.com>
2025-04-28 21:14:13 -0500

    added update movies

--- server/routes/movies.js
+++ server/routes/movies.js
@@ -3,4 +3,5 @@ const router = express.Router();
 const { getAllMovies, searchMovies } = require('../controllers/movieController');
 router.get('/', getAllMovies);
 router.get('/search', searchMovies);
+router.get('/movies/update', updateMovie);
 module.exports = router;
